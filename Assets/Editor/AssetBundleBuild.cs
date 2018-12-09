15e54061c7b5b3c3ac2fc06ccf55d505e278cb45
横田 准 <jyokota@r6.ucom.ne.jp>
2018-12-09 16:22:55 +0900

    first commit assets

---
+++ Assets/Editor/AssetBundleBuild.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+using UnityEditor;
+
+public class AssetBundleBuild : MonoBehaviour {
+
+	[MenuItem("Assets/Build AssetBundle")]
+	public static void Build () {
+		var assetBundleDirectory = "./AssetBundleBuild";
+		if (!Directory.Exists (assetBundleDirectory)) {
+			Directory.CreateDirectory (assetBundleDirectory);
+		}
+		BuildPipeline.BuildAssetBundles (assetBundleDirectory,
+			BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+		EditorUtility.DisplayDialog ("アセットバンドルビルド終了","アセットバンドルビルドが終わりました","OK");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/AssetBundleManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AssetBundleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Download(false));
+	}
+	
+	public IEnumerator Download (bool isLoacal) {
+
+		if (isLoacal) {
+
+			//ローカルからのロード
+			var ab = AssetBundle.LoadFromFile ("AssetBundleBuild/canvas");
+			if (ab == null) {
+				yield break;
+			}
+
+			var prefab = ab.LoadAsset<GameObject> ("Canvas");
+			Instantiate (prefab);
+		} else {
+			//サーバーからのロード
+			using (var wreq = UnityWebRequestAssetBundle.GetAssetBundle ("http://118.27.34.232/canvas")) {
+				yield return wreq.SendWebRequest ();
+				Debug.Log ("http response code:" + wreq.responseCode);
+				if (wreq.isNetworkError || wreq.isHttpError) {
+					Debug.LogError ("エラー" + wreq.error);
+					yield break;
+				}
+
+				var ab = DownloadHandlerAssetBundle.GetContent (wreq);
+				var prefab = ab.LoadAsset<GameObject> ("canvas");
+				Instantiate (prefab);
+			}
+		}
+
+		yield return null;
+	}
+
+	//ローカルからのロード
+
+
+	//サーバーからのロード
+}
