9ab863baef6e7416751f312ceb73e82d8cb2246b
jyokota <jyokota@r6.ucom.ne.jp>
2018-12-25 00:52:37 +0900

    add npoi.dll

---
+++ Assets/Editor/ExcelInport.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+
+public class ExcelInport : EditorWindow
+{
+
+    [MenuItem("Window/ExcelInport")]
+    static void Open()
+    {
+        GetWindow<ExcelInport>();
+    }
+
+    bool toggleValue;
+
+
+    void OnGUI()
+    {
+        EditorGUILayout.LabelField("ExcelInport");
+
+        EditorGUI.BeginChangeCheck();
+
+        //toggle をマウスでクリックして値を変更する
+        toggleValue = EditorGUILayout.ToggleLeft("Toggle", toggleValue);
+
+        //toggleValue の値が変更されるたびに true になる
+        if (EditorGUI.EndChangeCheck())
+        {
+
+
+
+            if (toggleValue)
+            {
+                Debug.Log("toggleValue が true になった瞬間呼び出される");
+            }
+
+        }
+    }
+}
