d834f62fb94e3682303680bad58ca5be94382b9e
jyokota <jyokota@r6.ucom.ne.jp>
2018-12-26 01:15:59 +0900

    Excelからマスターデータを読み込む準備

---
+++ Assets/Editor/ExcelImport.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+
+public class ExcelImport : EditorWindow
+{
+
+    [MenuItem("Window/ExcelImport")]
+    static void Open()
+    {
+        GetWindow<ExcelImport>();
+
+        Debug.Log(Application.dataPath);
+        
+        //DirectoryInfo dir = new DirectoryInfo(string.Format("{0}/Excel",Application.dataPath);
+        //FileInfo[] info = dir.GetFiles("*");
+
+        string[] files = Directory.GetFiles(string.Format("{0}/Excel", Application.dataPath), "*", System.IO.SearchOption.TopDirectoryOnly);
+
+        Debug.Log(files.Length);
+
+        foreach(string str in files)
+        {
+            Debug.Log(str);
+        }
+    }
+
+    bool toggleValue;
+
+
+    void OnGUI()
+    {
+        EditorGUILayout.LabelField("ExcelImport");
+
+       
+        if(GUILayout.Button("Button"))
+        {
+            Debug.Log("button clicked");
+        }
+        
+    }
+}
---
+++ Assets/Editor/QuestMaster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestMaster : ScriptableObject {
+
+    public List<Sheet> sheets = new List<Sheet>();
+
+    [System.SerializableAttribute]
+    public class Sheet
+    {
+        public string name = string.Empty;
+        public List<Param> list = new List<Param>();
+    }
+
+    [System.SerializableAttribute]
+    public class Param
+    {
+        public int id;
+        public string questname;
+        public string type;
+    }
+}
