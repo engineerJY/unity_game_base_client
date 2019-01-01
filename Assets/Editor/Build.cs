02f198075f06e0f9d2ce7430b49352f4916e946e
横田 准 <jyokota@r6.ucom.ne.jp>
2019-01-01 11:42:09 +0900

    modify build scripts.

---
+++ Assets/Editor/Build.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Build
+{
+    // ビルド実行でAndroidのapkを作成する例
+    [UnityEditor.MenuItem("Tools/Build Project AllScene Android")]
+    public static void BuildProjectAllSceneAndroid()
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+
+        List<string> allScene = new List<string>();
+
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (scene.enabled)
+            {
+                allScene.Add(scene.path);
+            }
+        }
+
+        PlayerSettings.applicationIdentifier = "com.yourcompany.newgame";
+        PlayerSettings.statusBarHidden = true;
+        BuildPipeline.BuildPlayer(
+            allScene.ToArray(),
+            "newgame.apk",
+            BuildTarget.Android,
+            BuildOptions.None
+        );
+    }
+}
