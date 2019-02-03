14153cdd325862a5cbecca5b2073ef3266532119
jyokota <jyokota@r6.ucom.ne.jp>
2019-02-03 18:15:10 +0900

    ScreenBase、SoundManager追加

---
+++ Assets/GameBaseClient/Scripts/ScreenBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBase : MonoBehaviour
+{
+    public virtual void Awake()
+    {
+        Debug.Log("ScreenBase:Awake");
+    }
+
+    public virtual void OnEnable()
+    {
+        Debug.Log("ScreenBase:OnEnable");
+    }
+
+    public virtual void Start()
+    {
+        Debug.Log("ScreenBase:Start");
+    }
+
+    public virtual void OnDesable()
+    {
+        Debug.Log("ScreenBase:OnDesable");
+    }
+
+    public virtual void OnDestroy()
+    {
+        Debug.Log("ScreenBase:OnDestroy");
+    }
+
+    void Update()
+    {
+        
+    }
+}
